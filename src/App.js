239e96a70d05283c12e1c4d0c1bbba85113406e3
Navyaarekuti <arekutinavya1@gmail.com>
2024-01-26 12:57:17 -0600

    All fILES

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import RewardManagement from './components/RewardManagement';
+import HeaderComponent from './components/HeaderComponent';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import AddReward from './components/AddReward';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <></>,
+  },
+  {
+    path: "/allrewards",
+    element: <>
+    <HeaderComponent />
+    <RewardManagement />
+    </>
+  },
+  {
+    path: "/addRewards",
+    element: <>
+    <AddReward />
+    </>
+  }
+]);
+
+  const App = () => {
+    return (
+      <RouterProvider router={router} />
+    );
+  };
+
 
 export default App;
---
+++ src/components/AddReward.js
@@ -0,0 +1,99 @@
+// import React, { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Link, useNavigate } from 'react-router-dom';
+import RewardManagement from './RewardManagement';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const AddReward = () => {
+  let navigate = useNavigate();
+
+  const [reward, setReward] = useState({
+    name: '',
+    description: '',
+    points: '',
+  });
+
+  const { name, description, points } = reward;
+
+  const onInputChange = (e) => {
+    setReward({ ...reward, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    //console.log("reward : ",reward)
+    await axios.post("http://localhost:8080/api/rewards/add", reward);
+    navigate('/allRewards'); 
+
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Add Reward</h2>
+
+          <form onSubmit={(e) => onSubmit(e)}>
+
+          <div className="mb-3">
+               <label htmlFor="Name" className="form-label">
+                  Reward Name
+               </label>
+               <input
+                 type={"text"}
+                 className="form-control"
+                 placeholder="Enter reward name"
+                 name="name"
+                 value={name}
+                 onChange={(e) => onInputChange(e)}
+               />
+             </div>
+             <div className="mb-3">
+               <label htmlFor="Description" className="form-label">
+                 Description
+               </label>
+               <input
+                 type={"text"}
+                 className="form-control"
+                 placeholder="Enter reward description"
+                 name="description"
+                 value={description}
+                 onChange={(e) => onInputChange(e)}
+               />
+             </div>
+             <div className="mb-3">
+               <label htmlFor="Points" className="form-label">
+                 Points
+               </label>
+               <input
+                 type={"number"}
+                 className="form-control"
+                 placeholder="Enter reward points"
+                 name="points"
+                 value={points}
+                 onChange={(e) => onInputChange(e)}
+              />
+             </div> 
+
+
+
+            <button type="submit" className="btn btn-outline-primary">
+              Save
+            </button>
+            <Link className="btn btn-outline-danger mx-2" to="/api/rewards/list'">
+              Discard
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddReward;
+
+
+
