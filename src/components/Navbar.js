0985092674a7ef379dec32784d0259f2d419b8f0
bhavana1308 <bonamukkala.bhavana@gmail.com>
2024-02-15 11:11:15 -0600

    code cleanup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,21 +7,27 @@ export default function Header(props) {
         <header className='header-body'>
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
-                <img src={require('../images/TC-sm-logo.png')} alt='Small Task Crusher Logo' className='header-logo'/>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                    <img src={require('../images/TC-sm-logo.png')} alt='Small Task Crusher Logo' className='header-logo' />
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="#">Add Child</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Create Chore</a>
+                                <a class="nav-link active" aria-current="page" href="/api/chores/add">Create Chore</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/api/chores/list">Chores List</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/api/assignments/assigned-chores/:id">Assigned Chores</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">
-                                Select Child
+                                    Select Child
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li><a class="dropdown-item" href="#">Child 1</a></li>
@@ -32,7 +38,7 @@ export default function Header(props) {
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="#">Logout</a>
                             </li>
-                        </ul>         
+                        </ul>
                     </div>
                 </div>
             </nav>
--- src/components/chores/AssignedChores.js
+++ src/components/chores/AssignedChores.js
@@ -1,18 +1,186 @@
 
+// import React, { useState, useEffect } from 'react';
+// import '../../styles/AssignedChoresStyles.css';
+// import { request, getAuthToken } from '../../axios_helper';
+// import getUserIdFromAuthToken from '../../axios_helper';
+// import Navbar from '../Navbar';
+
+
+// const AssignedChoresPage = () => {
+//   const [assignedChores, setAssignedChores] = useState([]);
+//   const id = getUserIdFromAuthToken(getAuthToken());
+
+//   const fetchAssignedChores = async () => {
+//     try {
+
+//       const response = await request('get', `api/assignments/assigned-chores/${id}`);
+
+//       if (response.status === 200) {
+//         const data = response.data;
+//         console.log(data);
+//         setAssignedChores(data);
+//       } else {
+//         console.error('Failed to fetch assigned chores');
+//       }
+//     } catch (error) {
+//       console.error('Failed to fetch assigned chores', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchAssignedChores();
+//   }, []);
+
+//   const handleDeleteChore = async (choreId) => {
+//     try {
+//       const response = await request('delete', `api/assignments/assigned-chores/${choreId}`);
+
+//       if (response.status === 200) {
+//         fetchAssignedChores();
+//         console.log('Assigned chore deleted successfully');
+//       } else {
+//         console.error('Failed to delete assigned chore');
+//       }
+//     } catch (error) {
+//       console.error('Error during chore deletion:', error.message);
+//     }
+//   };
+
+//   const handleApproveChore = async (choreId) => {
+//     try {
+//       const id = getUserIdFromAuthToken(getAuthToken());
+//       const response = await request('post', `api/status/approve/${choreId}/${id}`);
+
+//       if (response.status === 200) {
+//         console.log('Chore approved successfully');
+
+//         setAssignedChores((prevAssignedChores) => {
+//           const updatedChores = prevAssignedChores.map((item) => ({
+//             ...item,
+//             chores: item.chores.map((chore) =>
+//               chore.choreId === choreId ? { ...chore, status: 'APPROVED' } : chore
+//             ),
+//           }));
+//           return updatedChores;
+//         });
+//       } else {
+//         console.error('Failed to approve chore');
+//       }
+//     } catch (error) {
+//       console.error('Failed to approve chore', error.message);
+//     }
+//   };
+
+//   return (
+//     <div>
+
+//        <Navbar />
+
+//       <h1>Assigned Chores</h1>
+//       {Array.isArray(assignedChores) &&
+//         assignedChores.map((item) => (
+//           <div key={item.kid.kidId}>
+//             <h2>{item.kid.name}'s Chores</h2>
+//             <div className="flex-container">
+//               <div className="flex-section">
+//                 <h3>Assigned Chores</h3>
+//                 {item.chores
+//                   .filter((chore) => chore.status === 'ASSIGNED')
+//                   .map((chore) => (
+//                     <div key={chore.choreId} className="card-item">
+//                       <div>
+//                         <h4>{chore.name}</h4>
+//                         <p>{chore.description}</p>
+//                         <p>
+//                           <p>
+//                             <strong>Due Date:</strong> {chore.dueDate ? chore.dueDate.join('/') : ''}
+
+//                           </p>
+
+//                         </p>
+//                         <p>
+//                           <strong>Value Type:</strong> {chore.valueType}
+//                         </p>
+//                         <p>
+//                           <strong>Value:</strong> {chore.value}
+//                         </p>
+//                         <p>
+//                           <strong>Status:</strong> {chore.status}
+//                         </p>
+//                         <button className='button' onClick={() => handleDeleteChore(chore.choreId)}>Delete</button>
+//                       </div>
+//                     </div>
+//                   ))}
+//               </div>
+//               <div className="flex-section">
+//                 <h3>Waiting for Approval</h3>
+//                 {item.chores
+//                   .filter((chore) => chore.status === 'COMPLETED')
+//                   .map((chore) => (
+//                     <div key={chore.choreId} className="card-item">
+//                       <div>
+//                         <h4>{chore.name}</h4>
+//                         <p>{chore.description}</p>
+//                         <p>
+//                           <strong>Value Type:</strong> {chore.valueType}
+//                         </p>
+//                         <p>
+//                           <strong>Value:</strong> {chore.value}
+//                         </p>
+//                         <p>
+//                           <strong>Status:</strong> {chore.status}
+//                         </p>
+//                         {chore.status === 'COMPLETED' && (
+//                           <button type="submit" onClick={() => handleApproveChore(chore.choreId)}>Approve</button>
+//                         )}
+//                       </div>
+//                     </div>
+//                   ))}
+//               </div>
+//               <div className="flex-section">
+//                 <h3>Approved Chores</h3>
+//                 {item.chores
+//                   .filter((chore) => chore.status === 'APPROVED')
+//                   .map((chore) => (
+//                     <div key={chore.choreId} className="card-item">
+//                       <div>
+//                         <h4>{chore.name}</h4>
+//                         <p>{chore.description}</p>
+//                         <p>
+//                           <strong>Value Type:</strong> {chore.valueType}
+//                         </p>
+//                         <p>
+//                           <strong>Value:</strong> {chore.value}
+//                         </p>
+//                         <p>
+//                           <strong>Status:</strong> {chore.status}
+//                         </p>
+//                       </div>
+//                     </div>
+//                   ))}
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//     </div>
+//   );
+// };
+
+// export default AssignedChoresPage;
 import React, { useState, useEffect } from 'react';
 import '../../styles/AssignedChoresStyles.css';
 import { request, getAuthToken } from '../../axios_helper';
 import getUserIdFromAuthToken from '../../axios_helper';
 import Navbar from '../Navbar';
 
-
 const AssignedChoresPage = () => {
   const [assignedChores, setAssignedChores] = useState([]);
+  const [activeTab, setActiveTab] = useState('assigned'); // Default tab
+
   const id = getUserIdFromAuthToken(getAuthToken());
 
   const fetchAssignedChores = async () => {
     try {
-
       const response = await request('get', `api/assignments/assigned-chores/${id}`);
 
       if (response.status === 200) {
@@ -73,97 +241,104 @@ const AssignedChoresPage = () => {
 
   return (
     <div>
-    
-       <Navbar />
-      
+      <Navbar />
       <h1>Assigned Chores</h1>
+      <div className='btn-chores-container'>
+        <button className='btn btn-primary' onClick={() => setActiveTab('assigned')}>Assigned Chores</button>
+        <button className='btn btn-primary' onClick={() => setActiveTab('waiting')}>Waiting for Approval</button>
+        <button className='btn btn-primary' onClick={() => setActiveTab('approved')}>Approved Chores</button>
+      </div>
+
       {Array.isArray(assignedChores) &&
         assignedChores.map((item) => (
           <div key={item.kid.kidId}>
-            <h2>{item.kid.name}'s Chores</h2>
+            <h2>{item.kid.name}'s {activeTab === 'assigned' && 'Assigned Chores'}
+              {activeTab === 'waiting' && 'Waiting for Approval Chores'}
+              {activeTab === 'approved' && 'Approved Chores'}</h2>
             <div className="flex-container">
-              <div className="flex-section">
-                <h3>Assigned Chores</h3>
-                {item.chores
-                  .filter((chore) => chore.status === 'ASSIGNED')
-                  .map((chore) => (
-                    <div key={chore.choreId} className="card-item">
-                      <div>
-                        <h4>{chore.name}</h4>
-                        <p>{chore.description}</p>
-                        <p>
+              {/* Render content based on the active tab */}
+              {activeTab === 'assigned' && (
+                <div className="flex-section">
+                  {item.chores
+                    .filter((chore) => chore.status === 'ASSIGNED')
+                    .map((chore) => (
+                      <div key={chore.choreId} className="card-item">
+                        <div>
+                          <h4>{chore.name}</h4>
+                          <p>{chore.description}</p>
                           <p>
                             <strong>Due Date:</strong> {chore.dueDate ? chore.dueDate.join('/') : ''}
-
                           </p>
+                          <p>
+                            <strong>Value Type:</strong> {chore.valueType}
+                          </p>
+                          <p>
+                            <strong>Value:</strong> {chore.value}
+                          </p>
+                          <p>
+                            <strong>Status:</strong> {chore.status}
+                          </p>
+                          <button className='button' onClick={() => handleDeleteChore(chore.choreId)}>Delete</button>
+                        </div>
+                      </div>
+                    ))}
+                </div>
+              )}
 
-                        </p>
-                        <p>
-                          <strong>Value Type:</strong> {chore.valueType}
-                        </p>
-                        <p>
-                          <strong>Value:</strong> {chore.value}
-                        </p>
-                        <p>
-                          <strong>Status:</strong> {chore.status}
-                        </p>
-                        <button className='button' onClick={() => handleDeleteChore(chore.choreId)}>Delete</button>
+              {activeTab === 'waiting' && (
+                <div className="flex-section">
+                  {item.chores
+                    .filter((chore) => chore.status === 'COMPLETED')
+                    .map((chore) => (
+                      <div key={chore.choreId} className="card-item">
+                        <div>
+                          <h4>{chore.name}</h4>
+                          <p>{chore.description}</p>
+                          <p>
+                            <strong>Value Type:</strong> {chore.valueType}
+                          </p>
+                          <p>
+                            <strong>Value:</strong> {chore.value}
+                          </p>
+                          <p>
+                            <strong>Status:</strong> {chore.status}
+                          </p>
+                          {chore.status === 'COMPLETED' && (
+                            <button type="submit" onClick={() => handleApproveChore(chore.choreId)}>Approve</button>
+                          )}
+                        </div>
                       </div>
-                    </div>
-                  ))}
-              </div>
-              <div className="flex-section">
-                <h3>Waiting for Approval</h3>
-                {item.chores
-                  .filter((chore) => chore.status === 'COMPLETED')
-                  .map((chore) => (
-                    <div key={chore.choreId} className="card-item">
-                      <div>
-                        <h4>{chore.name}</h4>
-                        <p>{chore.description}</p>
-                        <p>
-                          <strong>Value Type:</strong> {chore.valueType}
-                        </p>
-                        <p>
-                          <strong>Value:</strong> {chore.value}
-                        </p>
-                        <p>
-                          <strong>Status:</strong> {chore.status}
-                        </p>
-                        {chore.status === 'COMPLETED' && (
-                          <button type="submit" onClick={() => handleApproveChore(chore.choreId)}>Approve</button>
-                        )}
+                    ))}
+                </div>
+              )}
+
+              {activeTab === 'approved' && (
+                <div className="flex-section">
+                  {item.chores
+                    .filter((chore) => chore.status === 'APPROVED')
+                    .map((chore) => (
+                      <div key={chore.choreId} className="card-item">
+                        <div>
+                          <h4>{chore.name}</h4>
+                          <p>{chore.description}</p>
+                          <p>
+                            <strong>Value Type:</strong> {chore.valueType}
+                          </p>
+                          <p>
+                            <strong>Value:</strong> {chore.value}
+                          </p>
+                          <p>
+                            <strong>Status:</strong> {chore.status}
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                  ))}
-              </div>
-              <div className="flex-section">
-                <h3>Approved Chores</h3>
-                {item.chores
-                  .filter((chore) => chore.status === 'APPROVED')
-                  .map((chore) => (
-                    <div key={chore.choreId} className="card-item">
-                      <div>
-                        <h4>{chore.name}</h4>
-                        <p>{chore.description}</p>
-                        <p>
-                          <strong>Value Type:</strong> {chore.valueType}
-                        </p>
-                        <p>
-                          <strong>Value:</strong> {chore.value}
-                        </p>
-                        <p>
-                          <strong>Status:</strong> {chore.status}
-                        </p>
-                      </div>
-                    </div>
-                  ))}
-              </div>
+                    ))}
+                </div>
+              )}
             </div>
           </div>
         ))}
     </div>
   );
 };
-
 export default AssignedChoresPage;
