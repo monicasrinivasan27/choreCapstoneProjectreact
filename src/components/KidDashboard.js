b57b890516814ac74b1f2f401e91f9638d9c4e0b
Navyaarekuti <arekutinavya1@gmail.com>
2024-03-10 12:27:25 -0500

    enhancemnets to kiddashboard

--- src/components/KidDashboard.js
+++ src/components/KidDashboard.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import KidNavbar from './KidNavbar';
+import { getChildUserNameFromAuthToken } from '../axios_helper';
+import  Kid from './kid/KidChores';
 
 const KidDashboard = () => {
 
+    const userName = getChildUserNameFromAuthToken();
     
         return (
             <div className='body'>
-                <KidNavbar />
+                <KidNavbar userName={userName}/>
                 <div className='jumbotron jumbotron'>
                     <div className='container'>
                         <h1 className='display-4'>Welcome</h1>
                         <p className='lead'>Let's earn some rewards!</p>
+                        <Kid userName={userName}></Kid>
                     </div>
                 </div>             
             </div>
